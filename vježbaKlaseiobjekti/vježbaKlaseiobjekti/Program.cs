9402d5149ccd6e6f0f619a5505003cd0bc2107d9
SeadColakovic <flegarsead263@gmail.com>
2024-12-09 13:32:44 +0100

    Add files via upload

---
+++ vježbaKlaseiobjekti/vježbaKlaseiobjekti/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace vježbaKlaseiobjekti
+{
+    class Zrakoplov
+    {
+        string marka;
+        string brojSasije;
+        int snaga;
+        int godinaProizvodnje;
+
+        public string BrojSasije { get => brojSasije; set => brojSasije = value; }
+        public int Snaga { get => snaga; set => snaga = value; }
+        public int GodinaProizvodnje { get => godinaProizvodnje; set => godinaProizvodnje = value; }
+
+        public void setMarka(string marka)
+        {
+
+            this.marka = marka;
+        }
+        public string getMarka()
+        {
+            return this.marka;
+        }
+        public Zrakoplov()
+        {
+
+        }
+        public Zrakoplov(string marka, string brojSasije, int snaga, int godinaProizvodnje)
+        
+        {
+            this.snaga = snaga;
+            this.marka = marka;
+            this.godinaProizvodnje = godinaProizvodnje;
+            this.BrojSasije = brojSasije;
+
+
+        }
+
+        public override string ToString()
+        {
+            string ispis = "Marka:" + this.marka + "\n Broj Šasije:" + this.brojSasije + "\n Snaga:" + this.snaga + "\n KS:" + "\n Godina proivodnje:" + this.godinaProizvodnje;
+        }
+    }
+    
+}
